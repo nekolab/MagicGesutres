72a6bbfb897aac9cb7341cbbaf04b08730b6b8fc
Sunny <sunny@magicgestures.org>
2013-04-02 18:46:08 +0800

    Add a lot of pre-definded gestures and function to lib.
    Now it has MagicGestures, Opera, FireGestures and SmoothGestures style.
    Also add full screen, maximize/minimize window function.
    Fix duplicate tab typo and use constant name style to name variable.
    
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/lib/predefinded.js
+++ src/chrome/lib/predefinded.js
@@ -1,27 +1,99 @@
 /**
  * @fileoverview This is an externs file provides predefinded contents.
- * @externs
+ * @author sunny@magicgestures.org {Sunny}
+ * @version 0.0.1.0
  */
 
-var preDefindedGestures = {
+var PRE_GESTURES = {
+    MagicGestures: {
+        L:     "history_back",
+        R:     "history_forward",
+        DR:    "close_tab",
+        UD:    "reload_tab",
+        LU:    "undo_close_tab",
+        UDU:   "reload_tab_bypass_cache",
+        URD:   "close_window",
+        UL:    "prev_tab",
+        UR:    "next_tab",
+        RUD:   "minimize_window",
+        RDU:   "maximize_window",
+        DU:    "goto_parent_dir",               //Not Supported Yet
+        LDR:   "view_current_frame_only",       //Not Supported Yet
+        LDRU:  "view_current_frame_in_new_tab", //Not Supported Yet
+        lDU:   "open_in_newtab_background",     //Not Supported Yet
+        lU:    "open_in_newtab",                //Not Supported Yet
+        wU:    "scroll_to_top",                 //Not Supported Yet
+        wD:    "scroll_to_bottom",              //Not Supported Yet
+        RDLUR: "open_options",                  //Not Supported Yet
+        RDLD:  "open_status"                    //Not Supported Yet
+    },
     Opera: {
-        L:   "history_back",
-        R:   "history_forward",
-        UL:  "goto_parent_dir",
-        UD:  "reload_tab",
-        U:   "stop_loading",
-        D:   "new_tab",
-        DU:  "duplicate_tab",
-        UR:  "maximize_tab", //Not Supported Yet
-        DL:  "minimize_tab", //Not Supported Yet
-        DR:  "close_tab",
-        RLR: "close_tab",
-        lD:  "open_in_newtab", //Not Supported Yet
-        lDU: "open_in_newtab_background", //Not Supported Yet
+        L:     "history_back",
+        R:     "history_forward",
+        UL:    "goto_parent_dir",               //Not Supported Yet
+        UD:    "reload_tab",
+        U:     "stop_loading",
+        D:     "new_tab",
+        DU:    "duplicate_tab",
+        UR:    "maximize_window",
+        DL:    "minimize_window",
+        DR:    "close_tab",
+        RLR:   "close_tab",
+        lD:    "open_in_newtab",                //Not Supported Yet
+        lDU:   "open_in_newtab_background",     //Not Supported Yet
+    },
+    FireGestures: {
+        L:     "history_back",
+        R:     "history_forward",
+        UD:    "reload_tab",
+        UDU:   "reload_tab_bypass_cache",
+        DU:    "goto_parent_dir",               //Not Supported Yet
+        DRU:   "new_window",
+        URD:   "close_window",
+        RUD:   "minimize_window",
+        RDU:   "maximize_window",
+        LR:    "new_tab",
+        DR:    "close_tab",
+        RL:    "undo_close_tab",
+        UL:    "prev_tab",
+        UR:    "next_tab",
+        LRU:   "zoom_in_text",                  //Not Supported Yet
+        LRD:   "zoom_out_text",                 //Not Supported Yet
+        LRUD:  "reset_text_zoom",               //Not Supported Yet
+        LU:    "scroll_to_top",                 //Not Supported Yet
+        LD:    "scroll_to_bottom",              //Not Supported Yet
+        LDR:   "view_current_frame_only",       //Not Supported Yet
+        LDRU:  "view_current_frame_in_new_tab", //Not Supported Yet
+        lD:    "open_in_newtab_background",     //Not Supported Yet
+        lU:    "open_in_newtab",                //Not Supported Yet
+        LDRUL: "open_options"                   //Not Supported Yet
+    },
+    SmoothGestures: {
+        U:     "new_tab",
+        lU:    "open_in_newtab",                //Not Supported Yet
+        D:     "toggle_pin_tab",
+        L:     "history_back",
+        R:     "history_forward",
+        UL:    "prev_tab",
+        UR:    "next_tab",
+        wU:    "scroll_to_top",                 //Not Supported Yet
+        wD:    "scroll_to_bottom",              //Not Supported Yet
+        DR:    "close_tab",
+        LU:    "undo_close_tab",
+        DU:    "duplicate_tab",
+        lDU:   "open_in_newtab_background",     //Not Supported Yet
+        UD:    "reload_tab",
+        UDU:   "reload_tab_bypass_cache",
+        URD:   "view_source",
+        UDR:   "split_tabs",
+        UDL:   "merge_tabs",                    //Not Supported Yet
+        LDR:   "list_cookies",                  //Not Supported Yet
+        RDLUR: "open_options",                  //Not Supported Yet
+        RDLD:  "open_status"                    //Not Supported Yet
     }
 };
 
-var preDefindedActions = {
+var PRE_ACTIONS = {
     close_tab: function(tab){
         chrome.tabs.remove(tab.id);
     },
@@ -56,13 +128,25 @@ var preDefindedActions = {
         chrome.windows.remove(tab.windowId);
     },
     duplicate_tab: function(tab){
-        chrome.tabs.duplicate()
+        chrome.tabs.duplicate(tab.id);
+    },
+    full_screen: function(tab){
+        chrome.windows.update(tab.windowId, {state: "fullscreen"});
     },
     history_back: function(tab){
         chrome.tabs.executeScript(tab.id, {code: "history.back();"});
     },
     history_forward: function(tab){
         chrome.tabs.executeScript(tab.id, {code: "history.forward();"});
+    },
+    maximize_window: function(tab){
+        var toggle = {maximized: "normal", normal: "maximized", fullscreen: "maximized", minimized: "normal"};
+        chrome.windows.get(tab.windowId, null, function(window){
+            chrome.windows.update(tab.windowId, {state: toggle[window.state]});
+        });
+    },
+    minimize_window: function(tab){
+        chrome.windows.update(tab.windowId, {state: "minimized"});
     },
     move_to_left: function(tab){
         chrome.tabs.move(tab.id, {index: 0});
