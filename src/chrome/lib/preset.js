b54a7c317d44652732670e26f6e61c859e555c85
Sunny <sunny@magicgestures.org>
2013-11-01 01:20:17 +0800

    Adjust layout to make preset.js more friendly.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/lib/preset.js
+++ src/chrome/lib/preset.js
@@ -9,11 +9,18 @@
 
 "use strict";
 
+/**
+ * Definition of MagicGestures.Preset.
+ */
 Object.defineProperty(MagicGestures, "Preset", {
     value: Object.create(null),
     enumerable: true
 });
 
+/**
+ * Preset profiles includes MagicGestures, FireGestures, SmoothGestures and Opera.
+ * Actions includes many default actions.
+ */
 Object.defineProperties(MagicGestures.Preset, {
     Profiles: {
         value: Object.create(null),
@@ -129,7 +136,6 @@ MagicGestures.Preset.Profiles.MagicGestures = function() {
     });
 };
 
-
 MagicGestures.Preset.Profiles.Opera = function() {
     return new MagicGestures.Profile({
         id: "af72c225f1d7ee4d",
@@ -198,7 +204,6 @@ MagicGestures.Preset.Profiles.Opera = function() {
         ]
     });
 };
-
 
 MagicGestures.Preset.Profiles.FireGestures = function() {
     return new MagicGestures.Profile({
@@ -432,15 +437,17 @@ MagicGestures.Preset.Profiles.SmoothGestures = function() {
 };
 
 Object.defineProperties(MagicGestures.Preset.Actions, {
+
     close_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.remove(tab.id);
         },
         enumerable: true
     },
+
     close_other_tabs: {
-        value: function(tab){
-            chrome.tabs.query({windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({windowId: tab.windowId}, function(tabs) {
                 for (var i = tabs.length - 1; i >= 0; i--) {
                     if (!tabs[i].pinned && tabs[i].id !== tab.id) {
                         chrome.tabs.remove(tabs[i].id);
@@ -450,9 +457,10 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     close_tabs_to_the_left: {
-        value: function(tab){
-            chrome.tabs.query({windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({windowId: tab.windowId}, function(tabs) {
                 for (var i = tabs.length - 1; i >= 0; i--) {
                     if (tabs[i].index < tab.index && !tabs[i].pinned) {
                         chrome.tabs.remove(tabs[i].id);
@@ -462,9 +470,10 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     close_tabs_to_the_right: {
-        value: function(tab){
-            chrome.tabs.query({windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({windowId: tab.windowId}, function(tabs) {
                 for (var i = tabs.length - 1; i >= 0; i--) {
                     if (tabs[i].index > tab.index && !tabs[i].pinned) {
                         chrome.tabs.remove(tabs[i].id);
@@ -474,136 +483,157 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     close_window: {
-        value: function(tab){
+        value: function(tab) {
             chrome.windows.remove(tab.windowId);
         },
         enumerable: true
     },
+
     duplicate_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.duplicate(tab.id);
         },
         enumerable: true
     },
+
     full_screen: {
-        value: function(tab){
+        value: function(tab) {
             chrome.windows.update(tab.windowId, {state: "fullscreen"});
         },
         enumerable: true
     },
+
     history_back: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {code: "history.back();"});
         },
         enumerable: true
     },
+
     history_forward: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {code: "history.forward();"});
         },
         enumerable: true
     },
+
     maximize_window: {
-        value: function(tab){
+        value: function(tab) {
             var toggle = {maximized: "normal", normal: "maximized", fullscreen: "maximized", minimized: "normal"};
-            chrome.windows.get(tab.windowId, null, function(window){
+            chrome.windows.get(tab.windowId, null, function(window) {
                 chrome.windows.update(tab.windowId, {state: toggle[window.state]});
             });
         },
         enumerable: true
     },
+
     minimize_window: {
-        value: function(tab){
+        value: function(tab) {
             chrome.windows.update(tab.windowId, {state: "minimized"});
         },
         enumerable: true
     },
+
     move_to_left: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.move(tab.id, {index: 0});
         },
         enumerable: true
     },
+
     move_to_right: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.move(tab.id, {index: -1});
         },
         enumerable: true
     },
+
     navigate_to_new_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.update(tab.id, {url: "chrome://newtab"});
         },
         enumerable: true
     },
+
     new_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1});
         },
         enumerable: true
     },
+
     new_window: {
-        value: function(){
+        value: function() {
             chrome.windows.create({});
         },
         enumerable: true
     },
+
     next_tab: {
-        value: function(tab){
-            chrome.tabs.query({index: tab.index + 1, windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({index: tab.index + 1, windowId: tab.windowId}, function(tabs) {
                 if (tabs.length !== 0) {chrome.tabs.update(tabs[0].id, {active: true});}
             });
         },
         enumerable: true
     },
+
     open_bookmarks: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1, url: "chrome://bookmarks/"});
         },
         enumerable: true
     },
+
     open_downloads: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1, url: "chrome://downloads/"});
         },
         enumerable: true
     },
+
     open_extensions: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1, url: "chrome://extensions/"});
         },
         enumerable: true
     },
+
     open_history: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1, url: "chrome://history/"});
         },
         enumerable: true
     },
+
     pin_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.update(tab.id, {pinned: true});
         },
         enumerable: true
     },
+
     prev_tab: {
-        value: function(tab){
-            chrome.tabs.query({index: tab.index - 1, windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({index: tab.index - 1, windowId: tab.windowId}, function(tabs) {
                 chrome.tabs.update(tabs[0].id, {active: true});
             });
         },
         enumerable: true
     },
+
     print_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {code: "window.print();"});
         },
         enumerable: true
     },
+
     reload_all_tabs: {
-        value: function(tab){
-            chrome.tabs.query({windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({windowId: tab.windowId}, function(tabs) {
                 for (var i = tabs.length - 1; i >= 0; i--) {
                     chrome.tabs.reload(tabs[i].id);
                 }
@@ -611,9 +641,10 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     reload_all_tabs_bypass_cache: {
-        value: function(tab){
-            chrome.tabs.query({windowId: tab.windowId}, function(tabs){
+        value: function(tab) {
+            chrome.tabs.query({windowId: tab.windowId}, function(tabs) {
                 for (var i = tabs.length - 1; i >= 0; i--) {
                     chrome.tabs.reload(tabs[i].id, {bypassCache: true});
                 }
@@ -621,25 +652,28 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     reload_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.reload(tab.id);
         },
         enumerable: true
     },
+
     reload_tab_bypass_cache: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.reload(tab.id, {bypassCache: true});
         },
         enumerable: true
     },
+
     split_tabs: {
-        value: function(tab){
-            chrome.windows.get(tab.windowId, {populate: true}, function(window){
+        value: function(tab) {
+            chrome.windows.get(tab.windowId, {populate: true}, function(window) {
                 chrome.windows.create({
                     focused: true, type: window.type,
                     incognito: window.incognito, tabId: tab.id
-                }, function(createdWindow){
+                }, function(createdWindow) {
                     for (var i = window.tabs.length - 1; i >= 0; i--) {
                         if (window.tabs[i].index > tab.index) {
                             chrome.tabs.move(window.tabs[i].id, {windowId: createdWindow.id, index: window.tabs[i].index - tab.index});
@@ -650,40 +684,46 @@ Object.defineProperties(MagicGestures.Preset.Actions, {
         },
         enumerable: true
     },
+
     stop_loading: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {code: "window.stop();"});
         },
         enumerable: true
     },
+
     scroll_to_top: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {code: "window.scroll(0, 0);"});
         },
         enumerable: true
     },
+
     scroll_to_bottom: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.executeScript(tab.id, {
                 code: "window.scrollTo(0, document.documentElement.scrollHeight);"
             });
         },
         enumerable: true
     },
+
     toggle_pin_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.update(tab.id, {pinned: !tab.pinned});
         },
         enumerable: true
     },
+
     unpin_tab: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.update(tab.id, {pinned: false});
         },
         enumerable: true
     },
+
     view_source: {
-        value: function(tab){
+        value: function(tab) {
             chrome.tabs.create({index: tab.index + 1, url: "view-source:" + tab.url, openerTabId: tab.id});
         },
         enumerable: true
