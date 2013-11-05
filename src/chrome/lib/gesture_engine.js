7155332413059d43ef92598fc985d23d7c504be1
Sunny <sunny@magicgestures.org>
2013-11-05 22:19:40 +0800

    Made some stupid mistake in NeuralNetEngine So fix it.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/lib/gesture_engine.js
+++ src/chrome/lib/gesture_engine.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Magic Gestures direction engine.
  * @author sunny@magicgestures.org {Sunny}
- * @version 0.0.0.4
+ * @version 0.0.0.5
  */
 
 /*global MagicGestures: true */
@@ -85,7 +85,7 @@ Object.defineProperty(MagicGestures, "directionEngine", {
 Object.defineProperty(MagicGestures, "NeuralNetEngine", {
     value: Object.create(null, {
         rebuildNetwork: function(neunetInfo) {
-            var network = function(details) {
+            var Network = function(details) {
                 this.inputCount = details.inputCount;
                 this.hiddenCount = details.hiddenCount;
                 this.outputCount = details.outputCount;
@@ -100,32 +100,33 @@ Object.defineProperty(MagicGestures, "NeuralNetEngine", {
                         throw "Not a vaild input for neural network engine.";
 
                     //Calculate hidden output
+                    var i, j;
                     var hiddenOutputs = [];
-                    for (var i = this.hiddenCount - 1; i >= 0; --i) {
+                    for (i = this.hiddenCount - 1; i >= 0; --i) {
                         var hiddenOutput = 0;
-                        hiddenOutput += hiddenWeights[(i + 1) * (this.inputCount + 1) - 1] * -1;
-                        for (var j = this.inputCount - 1; j >= 0; --j) {
-                            hiddenOutput += hiddenWeights[i * (this.inputCount + 1) + j] * inputs[j];
+                        hiddenOutput += this.hiddenWeights[(i + 1) * (this.inputCount + 1) - 1] * -1;
+                        for (j = this.inputCount - 1; j >= 0; --j) {
+                            hiddenOutput += this.hiddenWeights[i * (this.inputCount + 1) + j] * inputs[j];
                         }
                         hiddenOutputs.append(hiddenOutput);
                     }
 
                     //Calculate final output
                     var outputOutputs = [];
-                    for (var i = this.outputCount - 1; i >= 0; --i) {
+                    for (i = this.outputCount - 1; i >= 0; --i) {
                         var outputOutput = 0;
-                        outputOutput += outputWeights[(i + 1) * (this.hiddenCount + 1) - 1] * -1;
-                        for (var j = this.hiddenCount - 1; j >= 0; --j) {
-                            outputOutput += outputWeights[i * (this.hiddenCount + 1) + j] * hiddenOutputs[j];
+                        outputOutput += this.outputWeights[(i + 1) * (this.hiddenCount + 1) - 1] * -1;
+                        for (j = this.hiddenCount - 1; j >= 0; --j) {
+                            outputOutput += this.outputWeights[i * (this.hiddenCount + 1) + j] * hiddenOutputs[j];
                         }
                         outputOutputs.append(outputOutput);
                     }
 
-                    return outputActions[outputOutputs.indexOf(Math.max(outputOutputs))];
-                }
+                    return this.outputActions[outputOutputs.indexOf(Math.max(outputOutputs))];
+                };
             };
             
-            return new network(neunetInfo);
+            return new Network(neunetInfo);
         }
     })
 });
