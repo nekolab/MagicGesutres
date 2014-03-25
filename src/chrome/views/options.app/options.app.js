7c326ca3a8bd9eba0f95cd43f32527459192cbdf
Sunny <sunny@magicgestures.org>
2014-03-26 04:03:49 +0800

    Add animation support. Now we can see animation when switching tabs.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/views/options.app/options.app.js
+++ src/chrome/views/options.app/options.app.js
@@ -4,15 +4,18 @@ angular.module('options', ['ngRoute'])
             .when('/gestures', {
                 templateUrl: 'options.app/gestures.html',
                 controller: GesturesCtrl,
+                resolve: NavContrller.resolve,
                 activeTab: 'gestures'
             })
             .when('/settings', {
                 templateUrl: 'options.app/settings.html',
                 controller: SettingsCtrl,
+                resolve: NavContrller.resolve,
                 activeTab: 'settings'
             })
             .when('/help', {
                 templateUrl: 'options.app/help.html',
+                resolve: NavContrller.resolve,
                 activeTab: 'help'
             })
             .otherwise({redirectTo: '/settings'});
--- src/chrome/views/options.app/options.controller.js
+++ src/chrome/views/options.app/options.controller.js
@@ -1,11 +1,25 @@
-var GesturesCtrl = function($scope, $route){
+var GesturesCtrl = function($scope, $route) {
 
 };
 
-var SettingsCtrl = function($scope, $route){
+var SettingsCtrl = function($scope, $route) {
 
 };
 
-var NavContrller = function($scope, $route){
+var NavContrller = function($scope, $route) {
 	$scope.$route = $route;
+	$scope.$on('$routeChangeStart', function() {
+		$scope.isSelected = false;
+	});
+	$scope.viewRendered = function() {
+		$scope.isSelected = true;
+	};
+};
+
+NavContrller.resolve = {
+	delay: function($q, $timeout) {
+	    var delay = $q.defer();
+	    $timeout(delay.resolve, 100);
+	    return delay.promise;
+	}
 };
