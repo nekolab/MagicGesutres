1e4e7fd7818a762678dcc92ea243f026ffc47d95
Sunny <sunny@magicgestures.org>
2014-03-30 14:20:29 +0800

    A basic settings UI. Update model to fit UI. White space fix for controller. Introduce angular-csp stylesheet.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/lib/model.js
+++ src/chrome/lib/model.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Profile and Gesture model.
  * @author sunny@magicgestures.org {Sunny}
- * @version 0.0.2.1
+ * @version 0.0.2.4
  */
 
 /*global MagicGestures: true */
@@ -152,10 +152,17 @@ MagicGestures.Profile = function(profile) {
     this.triggerButton = 2;
 
     /**
-     * Indicate whether disable gesture when alt pressed or not.
+     * Disable mouse gesture temporary when specific key was pressed.
+     * Accept "Alt" and "Ctrl".
+     * @type {string}
+     */
+    this.disableWhen = "Alt";
+
+    /**
+     * Indicate "disable when specific key is pressed" is enabled or not.
      * @type {boolean}
      */
-    this.disableWhenAlt = false;
+    this.disableWhenPressed = false;
 
     /**
      * Indicate whether to draw the gesture locus or not.
@@ -172,11 +179,59 @@ MagicGestures.Profile = function(profile) {
     this.locusColor = [255, 255, 255, 1];
 
     /**
+     * Locus color visitor.
+     * Can parse string to number.
+     * All changes will be reflected on this.locusColor.
+     */
+    this.lineColor = Object.create(null);
+    var MagicProfile = this;
+    Object.defineProperties(this.lineColor, {
+        red: {
+            get: function() {
+                return MagicProfile.locusColor[0];
+            },
+            set: function(value) {
+                MagicProfile.locusColor[0] = parseFloat(value);
+            }
+        },
+        green: {
+            get: function() {
+                return MagicProfile.locusColor[1];
+            },
+            set: function(value) {
+                MagicProfile.locusColor[1] = parseFloat(value);
+            }
+        },
+        blue: {
+            get: function() {
+                return MagicProfile.locusColor[2];
+            },
+            set: function(value) {
+                MagicProfile.locusColor[2] = parseFloat(value);
+            }
+        },
+        alpha: {
+            get: function() {
+                return MagicProfile.locusColor[3];
+            },
+            set: function(value) {
+                MagicProfile.locusColor[3] = parseFloat(value);
+            }
+        }
+    });
+
+    /**
      * Indicate the width of locus.
      * Unit is px, default to 2.
      * @type {number}
      */
-    this.locusWidth = 2;
+    var locusWidth = 2;
+    this.__defineGetter__("locusWidth", function() {
+        return locusWidth;
+    });
+    this.__defineSetter__("locusWidth", function(value) {
+        locusWidth = parseFloat(value);
+    });
 
     /**
      * Locus time to live (ttl) means gesture will be canceled after specified seconds with no action.
@@ -184,6 +239,12 @@ MagicGestures.Profile = function(profile) {
      * @type {number}
      */
     this.ttl = 0;
+
+    /**
+     * Indicates whether ttl is enabled or not.
+     * @type {boolean}
+     */
+    this.ttlEnabled = false;
 
     /**
      * An array which store gestures.
@@ -213,7 +274,8 @@ MagicGestures.Profile = function(profile) {
     if (profile) {
         for (var item in profile) {
             if (this.hasOwnProperty(item)) {
-                this[item] = profile[item];
+                if (item !== "lineColor")
+                    this[item] = profile[item];
             } else {
                 MagicGestures.logging.warn("Not a vaild profile member:", item);
             }
--- src/chrome/views/options.app/options.controller.js
+++ src/chrome/views/options.app/options.controller.js
@@ -3,23 +3,32 @@ var GesturesCtrl = function($scope, $route) {
 };
 
 var SettingsCtrl = function($scope, $route) {
-
+    $scope.profileMap = MagicGestures.ProfileManager.profileMap;
+    $scope.selectedProfile = new MagicGestures.Profile(MagicGestures.ProfileManager.activedProfile);
+    $scope.activedProfile = new MagicGestures.Profile(MagicGestures.ProfileManager.activedProfile);
+    $scope.selectProfile = function(profileId) {
+        $scope.selectedProfile = new MagicGestures.Profile(MagicGestures.ProfileManager.profileMap[profileId]);
+    };
+    $scope.activeProfile = function(profileId) {
+        $scope.selectedProfile = new MagicGestures.Profile(MagicGestures.ProfileManager.profileMap[profileId]);
+        $scope.activedProfile = new MagicGestures.Profile(MagicGestures.ProfileManager.profileMap[profileId]);
+    };
 };
 
 var NavContrller = function($scope, $route) {
-	$scope.$route = $route;
-	$scope.$on('$routeChangeStart', function() {
-		$scope.isSelected = false;
-	});
-	$scope.viewRendered = function() {
-		$scope.isSelected = true;
-	};
+    $scope.$route = $route;
+    $scope.$on('$routeChangeStart', function() {
+        $scope.isSelected = false;
+    });
+    $scope.viewRendered = function() {
+        $scope.isSelected = true;
+    };
 };
 
 NavContrller.resolve = {
-	delay: function($q, $timeout) {
-	    var delay = $q.defer();
-	    $timeout(delay.resolve, 100);
-	    return delay.promise;
-	}
+    delay: function($q, $timeout) {
+        var delay = $q.defer();
+        $timeout(delay.resolve, 100);
+        return delay.promise;
+    }
 };
