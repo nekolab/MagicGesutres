ac4ed0cba70de1822fd1f0b9338ee95220892def
Sunny <sunny@magicgestures.org>
2014-03-30 14:25:41 +0800

    More rational directory sturcture.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/views/options.app/js/options.app.js
+++ src/chrome/views/options.app/js/options.app.js
@@ -2,19 +2,19 @@ angular.module('options', ['ngRoute'])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/gestures', {
-                templateUrl: 'options.app/gestures.html',
+                templateUrl: 'options.app/template/gestures.html',
                 controller: GesturesCtrl,
                 resolve: NavContrller.resolve,
                 activeTab: 'gestures'
             })
             .when('/settings', {
-                templateUrl: 'options.app/settings.html',
+                templateUrl: 'options.app/template/settings.html',
                 controller: SettingsCtrl,
                 resolve: NavContrller.resolve,
                 activeTab: 'settings'
             })
             .when('/help', {
-                templateUrl: 'options.app/help.html',
+                templateUrl: 'options.app/template/help.html',
                 resolve: NavContrller.resolve,
                 activeTab: 'help'
             })
