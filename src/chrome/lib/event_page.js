1a68b908761df5ebc45e750acdc20d43fe7cf72f
Sunny <sunny@magicgestures.org>
2014-04-15 22:02:34 +0800

    Rewrite MagicGestures.runtime.messenger Now you can use add/removeListener to add/remove listener for runtime message. Change event page to adapt new messenger.
    Signed-off-by: Sunny <sunny@magicgestures.org>

--- src/chrome/lib/event_page.js
+++ src/chrome/lib/event_page.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Magic Gestures event page script file.
  * @author sunny@magicgestures.org {Sunny}
- * @version 0.0.1.7
+ * @version 0.0.1.8
  */
 
 /*global chrome: false, MagicGestures: true */
@@ -9,39 +9,30 @@
 
 "use strict";
 
-/**
- * Implement MagicGestures.runtime.messenger.action
- */
-MagicGestures.runtime.messenger.action = function(type, msg, sender, sendResponse) {
-    switch(type) {
-        case "gesture ACTION":
-            MagicGestures.logging.debug(msg);
-            if (msg.command in MagicGestures.Preset.Actions) {
-                MagicGestures.Preset.Actions[msg.command].action.call(null, sender.tab, msg.data);
-            } else {
-                MagicGestures.logging.warn("Gesture action", msg.command, "doesn't support yet");
-            }
-            break;
-        case "page_action ACTION":
-            switch(msg.command) {
-                case "show":
-                    chrome.pageAction.show(sender.tab ? sender.tab.id : 0);
-                    break;
-                default:
-                    break;
-            }
-            break;
-        default:
-            MagicGestures.logging.debug("Cannot process", type, "type event.");
-            break;
-    }
-};
-
 MagicGestures.init = function() {
     MagicGestures.logging.log("Initializing MagicGestures...");
     MagicGestures.runtime.init("background");
     MagicGestures.ProfileManager.init();
     // TODO: Reload content script for each tab.
+
+    MagicGestures.runtime.messenger.addListener("gesture ACTION", function(msg, sender, sendResponse) {
+        MagicGestures.logging.debug(msg);
+        if (msg.command in MagicGestures.Preset.Actions) {
+            MagicGestures.Preset.Actions[msg.command].action.call(null, sender.tab, msg.data);
+        } else {
+            MagicGestures.logging.warn("Gesture action", msg.command, "doesn't support yet");
+        }
+    });
+
+    MagicGestures.runtime.messenger.addListener("page_action ACTION", function(msg, sender, sendResponse) {
+        switch(msg.command) {
+            case "show":
+                chrome.pageAction.show(sender.tab ? sender.tab.id : 0);
+                break;
+            default:
+                break;
+        }
+    });
 };
 
 chrome.runtime.onInstalled.addListener(function() {
